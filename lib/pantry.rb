826191ca952840074cb2196f1ab69323741b2dbc
Sarah Kirk <kirk710@gmail.com>
2017-04-18 09:36:46 -0600

    gets tests up and running

--- lib/pantry.rb
+++ lib/pantry.rb
@@ -1,3 +1,4 @@
 class Pantry
 
+def initialize
 end
--- test/pantry_test.rb
+++ test/pantry_test.rb
@@ -4,4 +4,9 @@ require 'minitest/pride'
 
 class PantryTest < Minitest::Test
 
+
+def test_init
+  p = Pantry.new
+  assert_instance_of Pantry, p
+end
 end
