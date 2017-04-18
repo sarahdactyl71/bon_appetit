d7810cf077048cea0567ee1644d58fc79b7b3af0
Sarah Kirk <kirk710@gmail.com>
2017-04-18 10:00:08 -0600

    adds more to the pantry class

--- lib/pantry.rb
+++ lib/pantry.rb
@@ -1,4 +1,20 @@
+require 'pry'
 class Pantry
 
-def initialize
+attr_reader :stock
+
+def initialize(stock= {})
+  @stock = stock
+end
+
+def stock_check(food)
+  if stock.include?(food)
+    stock.food
+end
+
+def restock(food, amount)
+  binding.pry
+  @stock[:food] = amount
+end
+
 end
--- test/pantry_test.rb
+++ test/pantry_test.rb
@@ -4,9 +4,24 @@ require 'minitest/pride'
 
 class PantryTest < Minitest::Test
 
+  attr_reader :pantry
 
-def test_init
-  p = Pantry.new
-  assert_instance_of Pantry, p
-end
+  def setup
+    @pantry = Pantry.new
+  end
+
+  def test_init
+    assert_instance_of Pantry, pantry
+  end
+
+  def test_can_we_check_stock
+    skip
+    assert_equal 0, pantry.stock_check("Cheese")
+  end
+
+  def test_can_we_restock
+    pantry.restock("Cheese", 10)
+    assert_equal 10, pantry.stock_check("Cheese")
+    assert_equal 30, pantry.restock("Cheese", 20)
+  end
 end
